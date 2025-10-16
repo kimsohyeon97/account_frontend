939190f5a10307aa5c84f42db31507ddcafd150e
kimsohyeon97 <sohyoun26@gmail.com>
2025-10-16 10:46:43 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+import Layout from "./components/layouts/Layout.vue";
+import Diary from "./pages/Diary.vue";
+import Calendar from "./pages/budget.vue";
+import Stats from "./pages/Stats.vue";
+import Setting from "./pages/Setting.vue";
+
+const view = ref("calendar");
+</script>
+
+<template>
+  <Layout @view="(val) => (view = val)">
+    <Diary v-if="view === 'calendar'" />
+    <Calendar v-if="view === 'budget'" />
+    <Stats v-if="view === 'stats'" />
+    <Setting v-if="view === 'setting'" />
+  </Layout>
+</template>
+
+<style scoped></style>
---
+++ src/components/layouts/Layout.vue
@@ -0,0 +1,78 @@
+<script setup>
+const emit = defineEmits(["view"]);
+
+function go(view) {
+  emit("view", view);
+}
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h4>가계부</h4>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+    <nav>
+      <button @click="go('calendar')">달력</button>
+      <!--안에 일지, 주별, 아이폰 calendar ui 참고-->
+      <button @click="go('budget')">예산</button>
+      <button @click="go('stats')">통계</button>
+      <button @click="go('setting')">설정</button>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+header {
+  position: relative;
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 12px 0;
+  border-bottom: 1px solid #eee;
+}
+
+header h4 {
+  margin: 0;
+  font-size: 16px;
+  font-weight: 600;
+}
+
+main {
+  flex: 1;
+  overflow: auto;
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 64px;
+  padding-bottom: env(safe-area-inset-bottom);
+}
+
+nav > button {
+  flex: 1;
+  height: 44px;
+  min-width: 0;
+  text-align: center;
+  line-height: 44px;
+  color: #bbb;
+  font-size: 14px;
+}
+
+nav > button:hover {
+  color: #333;
+}
+</style>
